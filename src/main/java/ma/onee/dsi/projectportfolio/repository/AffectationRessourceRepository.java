85b82ea74c8f7d39e24c96847f91339b92744016
zain-b69 <zainabbelkhou@outlook.com>
2026-07-25 13:52:33 +0100

    Prepare resource assignment repository

--- src/main/java/ma/onee/dsi/projectportfolio/repository/AffectationRessourceRepository.java
+++ src/main/java/ma/onee/dsi/projectportfolio/repository/AffectationRessourceRepository.java
@@ -1,11 +1,24 @@
 package ma.onee.dsi.projectportfolio.repository;
 
+import java.util.List;
+import java.util.Optional;
 import ma.onee.dsi.projectportfolio.entity.AffectationRessource;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AffectationRessourceRepository extends JpaRepository<AffectationRessource, Long> {
 
     long countByProjet_IdProjet(Long idProjet);
+
+    List<AffectationRessource> findByProjet_IdProjet(Long idProjet);
+
+    List<AffectationRessource> findByRessource_IdRessource(Long idRessource);
+
+    Optional<AffectationRessource> findByIdAffectationRessourceAndProjet_IdProjet(
+            Long idAffectationRessource,
+            Long idProjet
+    );
+
+    boolean existsByProjet_IdProjetAndRessource_IdRessource(Long idProjet, Long idRessource);
 
     boolean existsByRessource_IdRessource(Long idRessource);
 }
